93b4ec5cebc7c127bcbc2304e2a56ef092d652ab
Jared <jared@digitalnutt.com>
2020-01-08 15:10:42 -0800

    getting rid of extra

--- problem1/problem1.js
+++ problem1/problem1.js
@@ -1,5 +1,4 @@
 function multiplesOf3and5(number) {
-  let numbers = [];
   let total = 0;
   for (let i = 0; i < number; i++) {
     if (i % 3 == 0 || i % 5 == 0) {
@@ -8,7 +7,6 @@ function multiplesOf3and5(number) {
   }
 
   // Good luck!
-  console.log(total);
   return total;
 }
 
