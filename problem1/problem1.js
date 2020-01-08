a1bbed9beac37a337f53458d7e6b72b7c015eef2
Jared <jared@digitalnutt.com>
2020-01-08 15:15:16 -0800

    adding in equals sign

--- problem1/problem1.js
+++ problem1/problem1.js
@@ -1,6 +1,6 @@
 function multiplesOf3and5(number) {
   let total = 0;
-  for (let i = 0; i < number; i++) {
+  for (let i = 0; i <= number; i++) {
     if (i % 3 == 0 || i % 5 == 0) {
       total += i;
     }
