900f9e0cf6e512c0e2f39d1b0bb1a180fb5a6836
Jared <jarednutt@outlook.com>
2020-01-09 21:12:11 -0800

    adjusted a bit

--- problem3/problem3.js
+++ problem3/problem3.js
@@ -1,23 +1,24 @@
 function largestPrimeFactor(number) {
-  let factors = [];
+  // let factors = [];
   let primesAndFactor = [];
   // find all factors
   // In order to maintain this property of unique prime factorizations, it is necessary that the number one, 1, be categorized as neither prime nor composite.
+  // console.time("factorIterator loop")
   for (let factorIterator = 0; factorIterator <= number; factorIterator++) {
     // check if factor
     let isFactor = number % factorIterator == 0;
     let isPrime = true;
 
     if (isFactor) {
-      factors.push(factorIterator);
+      // factors.push(factorIterator);
 
       // see if factor is a prime
-      let highestFactor = 1;
       // a prime number has two factors, 1 and itself
       for (let i = 2; i < factorIterator; i++) {
         // console.log(i, factorIterator, factorIterator % i)
         if (factorIterator % i == 0) {
           isPrime = false;
+          continue;
         }
       }
     }
@@ -25,15 +26,12 @@ function largestPrimeFactor(number) {
     // if so, push to primes list
     if (isFactor && isPrime) {
       primesAndFactor.push(factorIterator);
-      console.log(factorIterator);
     }
-  }
+  } // end for loop
 
   // return last element of array
-  // console.log(factors)
-  console.log(primesAndFactor);
   // Good luck!
   return primesAndFactor.pop();
 }
 
-largestPrimeFactor(1092);
+largestPrimeFactor(13195);
