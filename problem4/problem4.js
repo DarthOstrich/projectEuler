f30a696a2b9281aacf492df18337debd065ca0a4
Jared <jarednutt@outlook.com>
2020-01-10 22:36:23 -0800

    problem 4

---
+++ problem4/problem4.js
@@ -0,0 +1,27 @@
+function isPalindrome(num) {
+  // also not my idea...
+  return Number([...`${num}`].reverse().join('')) === num;
+}
+
+function largestPalindromeProduct(n) {
+  let highestPalindrome = 0;
+  // Stole this
+  // Find largest number
+  let largestNum = '9';
+  largestNum += Number(largestNum.repeat(n - 1));
+  largestNum = Number(largestNum);
+
+  for (let i = largestNum; i > 0; i--) {
+    for (let j = largestNum; j > 0; j--) {
+      let product = i * j;
+      if (isPalindrome(product) && product > highestPalindrome)
+        highestPalindrome = product;
+    }
+  }
+
+  // Good luck!
+  console.log(highestPalindrome);
+  return highestPalindrome;
+}
+
+largestPalindromeProduct(3);
---
+++ problem4/problem4.md
@@ -0,0 +1,5 @@
+# References
+
+https://www.xarg.org/puzzle/project-euler/problem-4/
+
+https://github.com/Matt-1123/project-euler/blob/master/solutions.js
