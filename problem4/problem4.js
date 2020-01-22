96374454d066ee0cd27136f7b8daf1178420abbe
Jared <jared@digitalnutt.com>
2020-01-22 11:30:22 -0800

    adjusting problem 4

--- problem4/problem4.js
+++ problem4/problem4.js
@@ -8,7 +8,7 @@ function isPalindrome(num) {
   return Number(reversed) === num;
 }
 
-function largestPalindromeProduct(n) {
+function largestPalindromeProductNaive(n) {
   let highest = 0;
   // Find largest number
   let largestNum = '9';
@@ -23,7 +23,35 @@ function largestPalindromeProduct(n) {
   }
 
   // Good luck!
+  console.log(highest)
   return highest;
 }
 
+function largestPalindromeProduct(n) {
+  let highest = 0;
+  // Find largest number
+  let largestNum = '9';
+  largestNum += Number(largestNum.repeat(n - 1));
+  largestNum = Number(largestNum);
+
+  for (let i = largestNum; i > 0; i--) {
+    for (let j = largestNum; j > 0; j--) {
+      let product = i * j;
+      if (isPalindrome(product)) {
+        if (product > highest) highest = product;
+        // we have the highest palindrome for this set, skip to next loop
+        break;
+      }
+    }
+  }
+
+  // Good luck!
+  return highest;
+}
+
+console.time('pal');
+largestPalindromeProductNaive(3);
+console.timeEnd('pal');
+console.time('pal2');
 largestPalindromeProduct(3);
+console.timeEnd('pal2');
