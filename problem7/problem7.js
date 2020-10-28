5c8b1c547c81bf3f178043b86df2181de4328835
Jared <jarednutt@outlook.com>
2020-10-28 09:56:20 -0700

    adding in performance indicator

--- problem7/problem7.js
+++ problem7/problem7.js
@@ -1,3 +1,4 @@
+const { performance } = require("perf_hooks");
 function isPrime(num) {
   const isPrime = true;
   for (let i = 2; i < num; i++) {
@@ -8,7 +9,7 @@ function isPrime(num) {
   return true;
 }
 
-function nthPrime(n) {
+function nthPrimeNaive(n) {
   let count = 0;
   let highest = 0;
   for (let i = 2; count < n; i++) {
@@ -20,4 +21,11 @@ function nthPrime(n) {
   return highest;
 }
 
-nthPrime(1000); //7919
+function isPrimeAlgo(num) {}
+
+function nthPrimeAlgo(n) {}
+
+var t0 = performance.now();
+console.log("nthPrimeNaive:", nthPrimeNaive(10001));
+var t1 = performance.now();
+console.log("Took " + (t1 - t0) + " ms");
